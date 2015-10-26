7d37ac540e5863c8b235b8605dfbd6bb7e872b11
Kevin Price <kev.price83@gmail.com>
2015-10-26 11:23:32 +0100

    implement API endpoints, model logic and testing

---
+++ captain_train_api.rb
@@ -0,0 +1,145 @@
+require 'sinatra'
+require 'csv'
+require 'json'
+
+
+class StationCollection
+	CSV_PARAMS = { headers: true, col_sep: ";", encoding: "UTF-8" }
+  
+	def initialize
+		rows = CSV.read('stations.csv', CSV_PARAMS)
+		@stations = rows.map { |row| Station.new(row.to_hash) }.select { |s| s.valid? }
+	end
+
+  def random
+    @stations.sample
+  end
+
+  def find_named(name)
+    @stations.select { |station| station.name.match(name) }
+  end
+
+  def find_country(initials)
+    @stations.select { |station| station.country.match(initials) }
+  end
+
+  def by_distance(position)
+    @stations.map { |station| station.distance(position) }.sort
+  end
+
+  def size
+    @stations.size
+  end
+
+  def all
+    @stations
+  end
+end
+
+class Station
+	def initialize(attributes)
+		@attributes = attributes
+	end
+
+	def name
+		@attributes['name']
+	end
+
+  def country
+    @attributes['country']
+  end
+
+	def valid?
+		name
+	end
+
+	def to_json(options = {})
+		@attributes.to_json(options)
+	end
+
+  def position
+    lat_lon = @attributes.values_at('latitude', 'longitude')
+    Position.new(*lat_lon)
+  end
+
+  def distance(destination)
+    origin = self.position
+
+    rad_per_deg = Math::PI/180
+    rkm = 6371
+
+    dlat_rad = (destination.lat - origin.lat) * rad_per_deg
+    dlon_rad = (destination.lon - origin.lon) * rad_per_deg
+
+    lat1_rad = origin.lat * rad_per_deg
+    lat2_rad = destination.lat * rad_per_deg
+
+    a = Math.sin(dlat_rad/2)**2 + Math.cos(lat1_rad) * Math.cos(lat2_rad) * Math.sin(dlon_rad/2)**2
+    c = 2 * Math::atan2(Math::sqrt(a), Math::sqrt(1-a))
+
+    distance_in_km = rkm * c
+
+    Distance.new(distance_in_km, self)
+  end
+end
+
+class Position
+  attr_accessor :lat, :lon
+
+  def initialize(lat, lon)
+    @lat = lat.to_f
+    @lon = lon.to_f
+  end
+end
+
+class Distance
+  attr_reader :distance
+  
+  def initialize(distance, station)
+    @distance = distance
+    @station = station
+  end
+
+  def <=>(other)
+    @distance <=> other.distance
+  end
+
+  def to_json(options = {})
+    { distance: @distance, station: @station }.to_json(options)
+  end
+end
+
+stations = StationCollection.new
+
+before do
+  content_type :json
+end
+
+get '/.json' do 
+	
+  stations.to_json
+end
+
+get '/random.json' do
+	stations.random.to_json
+end
+
+get '/find/:name.json' do |name|
+  stations.find_named(name).to_json
+end
+
+get '/find/by_distance' do
+  lat_lon = params.values_at('latitude', 'longitude')
+  position = Position.new(*lat_lon)
+
+  stations.by_distance(position).take(10).to_json
+end
+
+# get '/find/:name.xml' do |name|
+#   content_type :xml
+#   stations.find_named(name).to_xml
+# end
+
+get '/find/country/:initials' do |initials|
+  stations.find_country(initials).to_json
+end
---
+++ csv_parser_test.rb
@@ -0,0 +1,64 @@
+require 'minitest/autorun'
+require_relative 'captain_train_api'
+
+class StationCollectionTest < Minitest::Test
+
+  @@station_collection = StationCollection.new
+  
+  def test_stations_size
+    assert_equal 20537, @@station_collection.size
+  end
+
+  def test_to_json
+    station = @@station_collection.all.first
+    assert_kind_of Station, station
+  end
+end
+
+class StationTest < Minitest::Test
+	def test_to_json
+		station = Station.new(name: 'my station')
+		assert_equal '{"name":"my station"}', station.to_json
+	end
+
+	def test_name
+		assert_equal 'foo', Station.new('name' => 'foo').name
+	end
+
+	def test_valid?
+		assert Station.new('name' => 'foo').valid?
+		refute Station.new({}).valid?
+	end
+
+	def test_position
+		station = Station.new('latitude' => '40.567', 'longitude' => '50.432')
+		assert_kind_of Position, station.position
+		assert_equal 40.567, station.position.lat
+		assert_equal 50.432, station.position.lon
+	end
+
+	def test_distance
+		station = Station.new('latitude' => '36.12', 'longitude' => '-86.67')
+		destination = Position.new(33.94, -118.4)
+
+		distance = station.distance(destination)
+		assert_equal 2886.4, distance.round(1)
+	end
+end
+
+
+class PositionTest < Minitest::Test
+  def test_new
+    position = Position.new('40.456', '-50.456')
+
+        assert_equal 40.456, position.lat
+        assert_equal (-50.456), position.lon
+  end
+end
+
+class DistanceTest < Minitest::Test
+	def test_to_json
+		distance = Distance.new(1337, { name: 'foo'})
+		assert_equal '{"distance":1337,"station":{"name":"foo"}}', distance.to_json
+	end
+end
